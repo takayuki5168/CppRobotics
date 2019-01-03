552b1dc4be31d48b226db4dc859b3bc5e02a094b
Takayuki Murooka <takayuki5168@gmail.com>
2019-01-03 18:18:01 +0900

    add PR2 DH parameters

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
 #include <array>
 #include <vector>
 #include "robotics/manipulation/links.hpp"
-#include "robotics/manipulation/abst_joint.hpp"
 #include "robotics/manipulation/rotational_joint.hpp"
 
 int main()
 {
   using namespace Robotics;
+  const double PI = 3.1416;
 
-  auto links = Links({RotationalJoint(1, 1, 1, 0),
-	RotationalJoint(1, 1, 1, 0)});
+  auto links = Links({RotationalJoint(0, -PI/2, .1, 0),
+	RotationalJoint(PI/2,  PI/2, 0, 0),
+	RotationalJoint(0, -PI/2, 0, .4),
+	RotationalJoint(0, PI/2, 0, 0),
+	RotationalJoint(0, -PI/2, 0, .321),
+	RotationalJoint(0, PI/2, 0, 0),
+	RotationalJoint(0, 0, 0, 0)});
   
   links.calcForwardKinematics();
   
